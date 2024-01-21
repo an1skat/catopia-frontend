1e5cd886791c2854b5d6e0aa024ba0a6e4857690
an1skat <156462349+an1skat@users.noreply.github.com>
2024-01-21 22:04:42 +0200

    Delete src directory

--- src/utils/CatopiaRedirect.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-const CatopiaRedirect = () => {
-    return <Navigate to="/auth/me" />;
-}
- 
-export default CatopiaRedirect;
--- src/utils/passwordVisibility.js
+++
@@ -1,7 +0,0 @@
-export const togglePasswordVisibility = (inputId) => {
-  const passwordInput = document.getElementById(inputId);
-  if (passwordInput) {
-    const type = passwordInput.type === "password" ? "text" : "password";
-    passwordInput.type = type;
-  }
-};
